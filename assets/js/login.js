61b66b3f578bd8bfa71ca3cea9f06eaf54f4e0cc
Donald Ferracci <dferracci@WebDev-MacBook-Pro15.local>
2018-06-22 17:57:08 -0400

    Added animation states to Login form.

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,7 +3,9 @@
 /* SCRIPTS TO REMOVE AND ADD CLASSES */
 /* href$ = The $ operator means 'ends with' */
 // REMOVE .pdfLink, ADD .nyroModal .pdfFile
-$("a:contains('Login')").addClass("login-reveal");
+// $("a:contains('Login')").addClass("login-reveal");
+
+$("a:contains('Login')").attr('id', 'login-panel');
 
 /* CLICK FUNCTIONS FOR TSP-9 AND TSP-15 Yes-No BUTTONS */
 // $(function () {
@@ -18,13 +20,21 @@ $("a:contains('Login')").addClass("login-reveal");
 //     });
 // });
 
-$(function () {
-    $(".login-reveal").click(function() {
-        // $("div.usa-overlay").toggleClass("is-visible");
-        $("#login").toggleClass("hide");
-    });
-});
+// $(function () {
+//     $(".login-reveal").click(function() {
+//         // $("div.usa-overlay").toggleClass("is-visible");
+//         $("#login").toggleClass("hide");
+//     });
+// });
 
+var logg = document.getElementById("login-panel");
+var fader = document.getElementById("login");
+
+logg.addEventListener("click", function() {
+
+  fader.classList.toggle("fadeIn");
+
+});
 
 // $(function () {
 //   var dialog = document.querySelector('dialog');
