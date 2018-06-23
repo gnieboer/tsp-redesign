74051955d48b3ed1aa475cb1dfb2769af88594cd
Donald Ferracci <dferracci@WebDev-MacBook-Pro15.local>
2018-06-23 18:55:01 -0400

    Added Close link for Login panel.

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,39 +2,47 @@
 
 /* SCRIPTS TO REMOVE AND ADD CLASSES */
 /* href$ = The $ operator means 'ends with' */
-// REMOVE .pdfLink, ADD .nyroModal .pdfFile
-// $("a:contains('Login')").addClass("login-reveal");
 
-$("a:contains('Login')").attr('id', 'login-panel');
+$("a:contains('Login')").attr('id', 'login-reveal').addClass("login-toggle");
 
-/* CLICK FUNCTIONS FOR TSP-9 AND TSP-15 Yes-No BUTTONS */
-// $(function () {
-//     $(".login-reveal").click(function () {
-//         $("#login").removeClass("hide")
-//     });
+var fader = document.getElementById("login-panel");
+var loggle = document.getElementsByClassName("login-toggle");
+
+var t;
+    for (t = 0; t < loggle.length; t++) {
+        loggle[t].addEventListener("click", function() {
+
+           fader.classList.toggle("fadeIn");
+
+});}
+
+// #login for the grab, .login for CSS, .login-reveal for combination with close button.
+
+// THIS WILL WORK WITH usa-overlay, BUT NOT WITH #login-panel
+
+// window.onclick = function(event) {
+//     if (event.target = fader) {
+//         // modal.style.display = "none";
+//         fader.classList.toggle("fadeIn");
+//     }
+// }
+
+
+
+
+// var logg = document.getElementById("login-reveal");
+// var fader = document.getElementById("login-panel");
+//
+// logg.addEventListener("click", function() {
+//
+//   fader.classList.toggle("fadeIn");
+//
 // });
 
-// $(function () {
-//     $(".login-reveal").click(function() {
-//         $("#login").toggleClass("hide");
-//     });
-// });
 
-// $(function () {
-//     $(".login-reveal").click(function() {
-//         // $("div.usa-overlay").toggleClass("is-visible");
-//         $("#login").toggleClass("hide");
-//     });
-// });
 
-var logg = document.getElementById("login-panel");
-var fader = document.getElementById("login");
 
-logg.addEventListener("click", function() {
-
-  fader.classList.toggle("fadeIn");
-
-});
+// <dialog> is not well-enough supported yet.
 
 // $(function () {
 //   var dialog = document.querySelector('dialog');
