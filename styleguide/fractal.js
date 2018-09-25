fd509be695e36c5d1e9bac3af40a5bc04e65da19
Scott Weber <stephen.weber@gsa.gov>
2018-09-25 08:15:13 -0400

    updates to fractal build

--- styleguide/fractal.js
+++ styleguide/fractal.js
@@ -39,4 +39,10 @@ const myCustomisedTheme = mandelbrot({
 
 fractal.web.theme(myCustomisedTheme); // tell Fractal to use the configured theme by default
 
+const builder = fractal.web.builder();
+
+builder.build().then(function(){
+    console.log(`Fractal static HTML build complete!`);
+});
+
 fractal.web.set('builder.dest', __dirname + '/build');
