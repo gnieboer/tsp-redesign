c7c91849a133b3e3c15c7c27de18c3fb1a12bc07
Scott Weber <scott.weber@gsa.gov>
2018-09-27 10:06:50 -0400

    Update fractal.js

--- styleguide/fractal.js
+++ styleguide/fractal.js
@@ -28,8 +28,8 @@ fractal.docs.set('path', path.join(__dirname, 'docs'));
 /*
  * Tell the Fractal web preview plugin where to look for static assets.
  */
-fractal.web.set('static.path', __dirname + '../styleguide/public');
-fractal.web.set('builder.dest', __dirname + '../styleguide/build');
+fractal.web.set('static.path', __dirname + '/public');
+fractal.web.set('builder.dest', __dirname + '/build');
 
 const builder = fractal.web.builder();
 
