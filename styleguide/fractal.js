f311777fba28063587082cf2d8e3424bc7401962
Scott Weber <scott.weber@gsa.gov>
2018-09-27 09:06:47 -0400

    Update fractal.js

--- styleguide/fractal.js
+++ styleguide/fractal.js
@@ -30,7 +30,7 @@ fractal.docs.set('path', path.join(__dirname, 'docs'));
  */
 fractal.web.set('static.path', path.join(__dirname, 'public'));
 
-fractal.web.set('builder.dest', '/styleguide/build');
+fractal.web.set('builder.dest', __dirname + '/styleguide');
 
 const builder = fractal.web.builder();
 
