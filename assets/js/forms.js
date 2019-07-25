c872a54c55ba0ef0ba803a568f8842b69a6f5ab7
William Haas <whaas@Williams-MacBook-Pro.local>
2019-07-25 16:31:30 -0500

    Remove console.log() lines.

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -11,13 +11,10 @@ function selectFormsTopic() {
 function showMoreForms(type, formNum) {
   var controlDiv = 'more-'+type+'-'+formNum;
   var contentDiv = 'more-'+type+'-content-'+formNum;
-console.log(type, formNum);
   if ($('#'+controlDiv).hasClass('see-less')) {
-console.log('add hide to '+contentDiv+'    remove see-less from '+controlDiv);
     $('#'+contentDiv).addClass('hide');
     $('#'+controlDiv).removeClass('see-less');
   } else {
-console.log('remove hide from '+contentDiv+'    add see-less to '+controlDiv);
     $('#'+contentDiv).removeClass('hide');
     $('#'+controlDiv).addClass('see-less');
   }
