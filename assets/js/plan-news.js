91c047b98df74e6a4d5e5eb4be77075e113518c5
William Haas <whaas@Williams-MacBook-Pro.local>
2019-11-14 15:17:52 -0600

    show more at bottom of list

--- assets/js/plan-news.js
+++ assets/js/plan-news.js
@@ -1,4 +1,22 @@
-function show() {
-   var element = document.getElementById("see-more-a");
+function show(prefix, suffix) {
+  var element = document.getElementById(prefix+'-'+suffix);
+  var accordion = document.getElementById(prefix+'-content-'+suffix);
+  if (element.hasClass('down')) {
+    accordion.show();
+  } else {
+    accordion.hide();
+  }
    element.classList.toggle("down");
+}
+
+function showMorePlans(type, formNum) {
+  var controlDiv = 'more-'+type+'-'+formNum;
+  var contentDiv = 'more-'+type+'-content-'+formNum;
+  if ($('#'+controlDiv).hasClass('down')) {
+    $('#'+contentDiv).addClass('hide');
+    $('#'+controlDiv).removeClass('down');
+  } else {
+    $('#'+contentDiv).removeClass('hide');
+    $('#'+controlDiv).addClass('down');
+  }
 }
