72f8255182681510ea8ed4d9dc61d8225a138764
William Haas <whaas@Williams-MacBook-Pro.local>
2020-02-24 01:34:36 -0600

    undo uniquenames test

--- assets/js/side-scroll-funds.js
+++ assets/js/side-scroll-funds.js
@@ -255,7 +255,7 @@ function fundHighchart(chartName, csvData, title, indexFundsFlag) {
       },
       title: { text: '' }
     },
-    xAxis: { uniqueNames: false },
+    // xAxis: { uniqueNames: false },
     tooltip: {
       formatter: function () {
         return fundTooltip(this, chartName);
