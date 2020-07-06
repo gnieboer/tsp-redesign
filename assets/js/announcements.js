cf3c3573ad5f608276e7c24566fea1bea897e33c
William Haas <whaas@Williams-MacBook-Pro.local>
2020-07-06 05:24:48 -0500

    search on individual announcement page.

--- assets/js/announcements.js
+++ assets/js/announcements.js
@@ -56,78 +56,36 @@ function showMoreAnnouncements(type, formNum) {
   }
 }
 
-
-
-
-
-// xxxxxxxxxxxxxxxxxxxxxx
-
-
-function show(prefix, suffix) {
-  var element = document.getElementById(prefix+'-'+suffix);
-  var accordion = document.getElementById(prefix+'-content-'+suffix);
-  if (element.hasClass('down')) {
-    accordion.show();
-  } else {
-    accordion.hide();
+// only call this on page load!
+function getCleanParm(parm, maxLength) {
+  var qs = getQueryString(parm);
+  if (typeof qs === 'undefined') { return ''; }
+  qs = decodeURIComponent(qs);
+  qs = qs.replace(/[^A-Z0-9_-]+/ig,' ');
+  qs = qs.substring(0,maxLength);
+  return qs;
+}
+// only call this on page load!
+function setQS(inputBox) {
+  var terms = getCleanParm('qs', 150);
+  if (terms.trim() != '') {
+    console.log('terms = '+terms);
+    $('#'+inputBox).val(terms).change();
+    return true;
   }
-   element.classList.toggle("down");
+  console.log('no terms = '+terms);
+  return false;
 }
 
-// share-bar callback
-function planNewsCallback(searchName, result) {
-  $('#'+'search-message-'+searchName).html('');
-  // console.log('in callback searchInlineCallback()');
-  // console.log(result);
-  // var jsonBlock = JSON.parse(result);
-  var jsonBlock = result;
-  var hits = jsonBlock.web.total;
-  var results = jsonBlock.web.results;
-  // console.log(searchName, results);
-  var items = [];
-  for (var i = 0; i < hits; i++) {
-    // var str = results[i].url.split(/\//).pop().replace(/.html$/, '');
-    var str = results[i].url.slice(0, -1).split(/\//).pop();
-    // console.log(str)
-    // console.log(results[i].url, str);
-    items.push(str);
+function searchOnAnnouncements(inputBox) {
+  var terms = $('#'+inputBox).val();
+  if (terms == '') {
+    // console.log('no search');
+    return false;
   }
-  // console.log(items.length);
-  showMatches(searchName, items);
-}
-
-// close accordions and show all content
-function resetInline(searchName) {
-  // console.log('reset '+searchName);
-  $('#'+searchName+'-default-div').show();
-  $('#'+searchName+'-search-results').hide();
-}
-// open accordions but hide all content
-function hideInline(searchName) {
-  // console.log('hide '+searchName);
-
-  // $('.plan-news-block').addClass('hide');
-  // open accordions
-  // hide accordion tops
-  $('#'+searchName+'-default-div').hide();
-  $('#'+searchName+'-search-results').show();
-  $('.'+searchName+'-search-block').addClass('hide');
-}
-// show plans that match, hide ones that document
-// expect array of plan item names
-function showMatches(searchName, items) {
-  hideInline(searchName);
-
-  if (items.length <= 0) {
-    // console.log('no matches '+searchName);
-    $('#search-message-'+searchName).html('no matches');
-    $('#'+searchName+'-search-results').hide();
-  }
-
-  // show winners
-  items.forEach(function (item, index) {
-    // console.log('processing ', item, index, '#'+item+'-1-block');
-    // $('#'+item+'-block').show();
-    $('#'+item+'-1-block').removeClass('hide');
-  });
+  terms = encodeURIComponent(terms);
+  // console.log('searching: '+terms);
+  var url = '/agency-service-reps/announcements/?qs='+terms;
+  location.href = url;
+  return true;
 }
--- pages/agency-service-reps/announcements.md
+++ pages/agency-service-reps/announcements.md
@@ -4,14 +4,18 @@ title: Announcements
 styles:
 sidenav: agency-service-reps
 scripts:
+  - /assets/js/bootstrap.min.js
+  - /assets/js/copy-to-clipboard.js
   - /assets/js/forms.js
+  - /assets/js/calculator/calculator.js
   - /assets/js/announcements.js
-  - /assets/js/ajax-usa-search-gov.js
 permalink: /agency-service-reps/announcements/
 # Jekyll redirects https://github.com/jekyll/jekyll-redirect-from
 redirect_from:
   - /representative/Content/announcements.html
   - /representative/Content/announcementsArchive.html
+document-ready:
+  - setQS('search-input-announcements');
 ---
 
 # Announcements
